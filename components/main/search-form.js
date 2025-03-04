fd7562920d484b102feb096ab3b7acf5030e17e9
jiyoungnim <cloundyon31@gmail.com>
2025-03-04 22:10:36 +0900

    💄 검색창 스타일링 완료

--- components/main/search-form.js
+++ components/main/search-form.js
@@ -4,19 +4,31 @@ import classes from "./search-form.module.css";
 export default function SearchForm() {
   return (
     <form className={classes.searchForm}>
-      <button>
-        <div className={classes.searchButton}>
-          <span>중고거래</span>
-          <Image src="/icons/triangle.svg" alt="image" width={18} height={18} />
-        </div>
+      <button className={`${classes.buttonUnset} ${classes.locationBtn}`}>
+        <span>위치</span>
+        <Image src="/icons/location.svg" alt="image" width={18} height={18} />
       </button>
-      <div className={classes["search-input-wrapper"]}>
-        <input
-          type="text"
-          className={classes.searchInput}
-          placeholder="Search"
-        />
-        <button>
+
+      <div className={classes.wrapper}>
+        <div className={classes.categoryButton}>
+          <span>중고거래</span>
+          <Image
+            src="/icons/chevron-down.svg"
+            alt="image"
+            width={18}
+            height={18}
+          />
+        </div>
+
+        <div className={classes.searchFlexGrow}>
+          <input
+            type="text"
+            className={`${classes.inputUnset} ${classes.searchInput}`}
+            placeholder="Search"
+          />
+        </div>
+
+        <button className={`${classes.buttonUnset} ${classes.searchBtn}`}>
           <Image src="/icons/search.svg" alt="image" width={24} height={24} />
         </button>
       </div>
