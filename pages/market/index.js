7181ec0b6cdcce328e7b1b69b0390049a3cd08ca
jiyoungnim <cloundyon31@gmail.com>
2025-03-06 23:57:35 +0900

    📝 Market 페이지 내 게시물 리스트 마크업 완료 (posts-list.js)

--- pages/market/index.js
+++ pages/market/index.js
@@ -1,6 +1,4 @@
 import SubHeader from "@/components/layout/sub-header";
-import MainHeader from "@/components/main/main-header";
-import SearchForm from "@/components/main/search-form";
 import PostsList from "@/components/market/posts-list";
 
 export default function MarketPage() {
