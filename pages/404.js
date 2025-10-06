d4911f67df371557dab4fad4adc5e8366c6279ec
jiyoungnim <cloundyon31@gmail.com>
2025-10-06 15:08:42 +0900

    💄 style: 404 오류 페이지에 들어갈 파비콘 그림 생성 및 적용

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 // pages/404.js (또는 .tsx)
 export default function Custom404() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <Image
+        src="/images/404-favicon.png"
+        alt="icon"
+        width={200}
+        height={200}
+        className="mb-10"
+      />
       <h1 className="text-4xl font-bold mb-4">
         404 - 페이지를 찾을 수 없습니다
       </h1>
